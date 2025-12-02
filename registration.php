6ec27fea1ca7fcb5704c2ce73d4ff1c79ae6cfea
xboyraa007 <heatmenghuot@gmail.com>
2025-12-02 09:48:35 +0700

    Add files via upload

---
+++ registration.php
@@ -0,0 +1,242 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+
+session_start(); 
+error_reporting(0); 
+include("connection/connect.php"); 
+if(isset($_POST['submit'] )) 
+{
+     if(empty($_POST['firstname']) || 
+   	    empty($_POST['lastname'])|| 
+		empty($_POST['email']) ||  
+		empty($_POST['phone'])||
+		empty($_POST['password'])||
+		empty($_POST['cpassword']) ||
+		empty($_POST['cpassword']))
+		{
+			$message = "All fields must be Required!";
+		}
+	else
+	{
+	
+	$check_username= mysqli_query($db, "SELECT username FROM users where username = '".$_POST['username']."' ");
+	$check_email = mysqli_query($db, "SELECT email FROM users where email = '".$_POST['email']."' ");
+		
+
+	
+	if($_POST['password'] != $_POST['cpassword']){  
+       	
+          echo "<script>alert('Password not match');</script>"; 
+    }
+	elseif(strlen($_POST['password']) < 6)  
+	{
+      echo "<script>alert('Password Must be >=6');</script>"; 
+	}
+	elseif(strlen($_POST['phone']) < 9)  
+	{
+      echo "<script>alert('Invalid phone number!');</script>"; 
+	}
+
+    elseif (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) 
+    {
+          echo "<script>alert('Invalid email address please type a valid email!');</script>"; 
+    }
+	elseif(mysqli_num_rows($check_username) > 0) 
+     {
+       echo "<script>alert('Username Already exists!');</script>"; 
+     }
+	elseif(mysqli_num_rows($check_email) > 0) 
+     {
+       echo "<script>alert('Email Already exists!');</script>"; 
+     }
+	else{
+       
+	 
+	$mql = "INSERT INTO users(username,f_name,l_name,email,phone,password,address) VALUES('".$_POST['username']."','".$_POST['firstname']."','".$_POST['lastname']."','".$_POST['email']."','".$_POST['phone']."','".md5($_POST['password'])."','".$_POST['address']."')";
+	mysqli_query($db, $mql);
+	
+		 header("refresh:0.1;url=login.php");
+    }
+	}
+
+}
+
+
+?>
+
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="#">
+    <title>Registration</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/animsition.min.css" rel="stylesheet">
+    <link href="css/animate.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet"> </head>
+<body>
+<style> 
+              @font-face {
+                            font-family: myFirstFont;
+                            src: url(fonts/KhRayuthHD20.ttf);
+                        }
+
+                      * {
+                           font-family: myFirstFont;
+                        }
+        </style>
+<div style=" background-image: url('images/img/back.jpg');">
+         <header id="header" class="header-scroll top-header headrom">
+            <nav class="navbar navbar-dark">
+               <div class="container">
+                  <button class="navbar-toggler hidden-lg-up" type="button" data-toggle="collapse" data-target="#mainNavbarCollapse">&#9776;</button>
+                  <a class="navbar-brand" href="index.php"> <img class="img-rounded" src="images/sto.png" alt=""> </a>
+                  <div class="collapse navbar-toggleable-md  float-lg-right" id="mainNavbarCollapse">
+                     <ul class="nav navbar-nav">
+                     <li class="nav-item"> <a class="nav-link active" href="index.php">👑 ទំព័រដើម <span class="sr-only">(current)</span></a> </li>
+                            
+                           
+                            <?php
+                         if(empty($_SESSION["user_id"])) // if user is not login
+                            {
+                               echo '<li class="nav-item"><a href="login.php" class="nav-link active">🔑 ចូលគណនី</a> </li>
+                              <li class="nav-item"><a href="registration.php" class="nav-link active">📝 បង្កើតគណនី</a> </li>';
+                            }
+                         else
+                            {
+       
+                                  
+                                  echo  '<li class="nav-item"><a href="your_orders.php" class="nav-link active">🎟️ ការបញ្ជាទិញ</a> </li>';
+                                  echo  '<li class="nav-item"><a href="logout.php" class="nav-link active">🚀 ចាកចេញពីគណនី</a> </li>';
+                            }
+       
+                         ?>
+							 
+                        </ul>
+                  </div>
+               </div>
+            </nav>
+         </header>
+         <div class="page-wrapper">
+            
+               <div class="container">
+                  <ul>
+                    
+                    
+                  </ul>
+               </div>
+            
+            <section class="contact-page inner-page">
+               <div class="container ">
+                  <div class="row ">
+                     <div class="col-md-12">
+                        <div class="widget" >
+                           <div class="widget-body">
+                            
+							  <form action="" method="post">
+                                 <div class="row">
+								  <div class="form-group col-sm-12">
+                                       <label for="exampleInputEmail1">ឈ្មោះអ្នកប្រើ</label>
+                                       <input class="form-control" type="text" name="username" id="example-text-input"> 
+                                    </div>
+                                    <div class="form-group col-sm-6">
+                                       <label for="exampleInputEmail1">ឈ្មោះដំបូង</label>
+                                       <input class="form-control" type="text" name="firstname" id="example-text-input"> 
+                                    </div>
+                                    <div class="form-group col-sm-6">
+                                       <label for="exampleInputEmail1">នាមត្រកូល</label>
+                                       <input class="form-control" type="text" name="lastname" id="example-text-input-2"> 
+                                    </div>
+                                    <div class="form-group col-sm-6">
+                                       <label for="exampleInputEmail1">អ៊ីមែល-Email</label>
+                                       <input type="email" class="form-control" name="email" id="exampleInputEmail1" aria-describedby="emailHelp"> 
+                                    </div>
+                                    <div class="form-group col-sm-6">
+                                       <label for="exampleInputEmail1">លេខទូរស័ព្ទ</label>
+                                       <input class="form-control" type="number" name="phone" id="example-tel-input-3"> 
+                                    </div>
+                                    <div class="form-group col-sm-6">
+                                       <label for="exampleInputPassword1">ពាក្យសម្ងាត់</label>
+                                       <input type="password" class="form-control" name="password" id="exampleInputPassword1"> 
+                                    </div>
+                                    <div class="form-group col-sm-6">
+                                       <label for="exampleInputPassword1">បញ្ជាក់ពាក្យសម្ងាត់</label>
+                                       <input type="password" class="form-control" name="cpassword" id="exampleInputPassword2"> 
+                                    </div>
+									 <div class="form-group col-sm-12">
+                                       <label for="exampleTextarea">អាសយដ្ឋានដឹកជញ្ជូន</label>
+                                       <textarea class="form-control" id="exampleTextarea"  name="address" rows="3"></textarea>
+                                    </div>
+                                   
+                                 </div>
+                                
+                                 <div class="row">
+                                    <div class="col-sm-4">
+                                       <p> <input type="submit" value="Register" name="submit" class="btn theme-btn"> </p>
+                                    </div>
+                                 </div>
+                              </form>
+                  
+						   </div>
+           
+                        </div>
+                     
+                     </div>
+                    
+                  </div>
+               </div>
+            </section>
+            
+      
+            <footer class="footer">
+            <div class="container">
+                
+          
+                <div class="bottom-footer">
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-3 payment-options color-gray">
+                            <h5 style="font-family:myFirstFont">ជម្រើសបង់ប្រាក់</h5>
+                            <ul>
+                                <li>
+                                    <a href="#"> <img src="images/paypal.png" alt="Paypal"> </a>
+                                </li>
+                                <li>
+                                    <a href="#"> <img src="images/mastercard.png" alt="Mastercard"> </a>
+                                </li>
+                                <li>
+                                    <a href="#"> <img src="images/khqr.png" alt="KH QR" width="45" height="25"> </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-xs-12 col-sm-4 address color-gray">
+                                    <h5 style="font-family:myFirstFont">អាស័យដ្ឋាន</h5>
+                                    <p style="font-family:myFirstFont">កម្ពុជា , ក្រុងប៉ោយប៉ែត</p>
+                                    <h5 style="font-family:myFirstFont">លេខទរសព្វ: <a style="font-family:myFirstFont"> +855 713323326</a></a></h5> </div>
+                                <div class="col-xs-12 col-sm-5 additional-info color-gray">
+                                    <h5 style="font-family:myFirstFont">ពត៌មានបន្ថែម</h5>
+                                   <p style="font-family:myFirstFont">អរគុណសម្រាប់ការគាំទ្រហាងយើងខ្ញុំ។</p>
+                                </div>
+                    </div>
+                </div>
+          
+            </div>
+        </footer>
+         
+         </div>
+       
+    <script src="js/jquery.min.js"></script>
+    <script src="js/tether.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/animsition.min.js"></script>
+    <script src="js/bootstrap-slider.min.js"></script>
+    <script src="js/jquery.isotope.min.js"></script>
+    <script src="js/headroom.js"></script>
+    <script src="js/foodpicky.min.js"></script>
+</body>
+
+</html>
---
+++ your_orders.php
@@ -0,0 +1,378 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+include("connection/connect.php");
+error_reporting(0);
+session_start();
+
+if(empty($_SESSION['user_id']))  
+{
+	header('location:login.php');
+}
+else
+{
+?>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="#">
+    <title>ការបញ្ជាទិញ?</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/animsition.min.css" rel="stylesheet">
+    <link href="css/animate.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+<style type="text/css" rel="stylesheet">
+	
+
+
+.indent-small {
+  margin-left: 5px;
+}
+.form-group.internal {
+  margin-bottom: 0;
+}
+.dialog-panel {
+  margin: 10px;
+}
+.datepicker-dropdown {
+  z-index: 200 !important;
+}
+.panel-body {
+  background: #e5e5e5;
+  /* Old browsers */
+  background: -moz-radial-gradient(center, ellipse cover, #e5e5e5 0%, #ffffff 100%);
+  /* FF3.6+ */
+  background: -webkit-gradient(radial, center center, 0px, center center, 100%, color-stop(0%, #e5e5e5), color-stop(100%, #ffffff));
+  /* Chrome,Safari4+ */
+  background: -webkit-radial-gradient(center, ellipse cover, #e5e5e5 0%, #ffffff 100%);
+  /* Chrome10+,Safari5.1+ */
+  background: -o-radial-gradient(center, ellipse cover, #e5e5e5 0%, #ffffff 100%);
+  /* Opera 12+ */
+  background: -ms-radial-gradient(center, ellipse cover, #e5e5e5 0%, #ffffff 100%);
+  /* IE10+ */
+  background: radial-gradient(ellipse at center, #e5e5e5 0%, #ffffff 100%);
+  /* W3C */
+  filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#e5e5e5', endColorstr='#ffffff', GradientType=1);
+  font: 600 15px "Open Sans", Arial, sans-serif;
+}
+label.control-label {
+  font-weight: 600;
+  color: #777;
+}
+
+/* 
+table { 
+	width: 750px; 
+	border-collapse: collapse; 
+	margin: auto;
+	
+	}
+
+/* Zebra striping */
+/* tr:nth-of-type(odd) { 
+	background: #eee; 
+	}
+
+th { 
+	background: #404040; 
+	color: white; 
+	font-weight: bold; 
+	
+	}
+
+td, th { 
+	padding: 10px; 
+	border: 1px solid #ccc; 
+	text-align: left; 
+	font-size: 14px;
+	
+	} */ */
+
+
+@media 
+only screen and (max-width: 760px),
+(min-device-width: 768px) and (max-device-width: 1024px)  {
+
+	/* table { 
+	  	width: 100%; 
+	}
+
+	
+	table, thead, tbody, th, td, tr { 
+		display: block; 
+	} */
+	
+	
+	/* thead tr { 
+		position: absolute;
+		top: -9999px;
+		left: -9999px;
+	}
+	
+	tr { border: 1px solid #ccc; } */
+	
+	/* td { 
+		
+		border: none;
+		border-bottom: 1px solid #eee; 
+		position: relative;
+		padding-left: 50%; 
+	}
+
+	td:before { 
+		
+		position: absolute;
+	
+		top: 6px;
+		left: 6px;
+		width: 45%; 
+		padding-right: 10px; 
+		white-space: nowrap;
+		
+		content: attr(data-column);
+
+		color: #000;
+		font-weight: bold;
+	} */
+
+}
+
+
+
+
+
+
+
+	</style>
+
+	</head>
+
+<body>
+<style> 
+              @font-face {
+                            font-family: myFirstFont;
+                            src: url(fonts/KhRayuthHD20.ttf);
+                        }
+
+                      * {
+                           font-family: myFirstFont;
+                        }
+        </style>
+      
+        <header id="header" class="header-scroll top-header headrom">
+  
+            <nav class="navbar navbar-dark">
+                <div class="container">
+                    <button class="navbar-toggler hidden-lg-up" type="button" data-toggle="collapse" data-target="#mainNavbarCollapse">&#9776;</button>
+                    <a class="navbar-brand" href="index.php"> <img class="img-rounded" src="images/sto.png" alt=""> </a>
+                    <div class="collapse navbar-toggleable-md  float-lg-right" id="mainNavbarCollapse">
+                        <ul class="nav navbar-nav">
+                        <li class="nav-item"> <a class="nav-link active" href="index.php">👑 ទំព័រដើម <span class="sr-only">(current)</span></a> </li>
+                            
+                           
+							<?php
+						if(empty($_SESSION["user_id"])) // if user is not login
+							{
+								echo '<li class="nav-item"><a href="login.php" class="nav-link active">🔑 ចូលគណនី</a> </li>
+                              <li class="nav-item"><a href="registration.php" class="nav-link active">📝 បង្កើតគណនី</a> </li>';
+							}
+						else
+							{
+
+									
+									echo  '<li class="nav-item"><a href="your_orders.php" class="nav-link active">🎟️ ការបញ្ជាទិញ</a> </li>';
+                                  echo  '<li class="nav-item"><a href="logout.php" class="nav-link active">🚀 ចាកចេញពីគណនី</a> </li>';
+							}
+
+						?>
+							 
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+  
+        </header>
+        <div class="page-wrapper">
+       
+           
+    
+            <div class="inner-page-hero bg-image" data-image-src="images/img/back.jpg">
+                <div class="container"> </div>
+        
+            </div>
+            <div class="result-show">
+                <div class="container">
+                    <div class="row">
+                       
+                       
+                    </div>
+                </div>
+            </div>
+    
+            <section class="restaurants-page">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xs-12">
+                          </div>
+                        <div class="col-xs-12">
+                            <div class="bg-gray">
+                                <div class="row">
+								
+							<table class="table table-bordered table-hover">
+						  <thead style = "background: #404040; color:white;">
+							<tr>
+							
+							  <th>Item</th>
+							  <th>Quantity</th>
+							  <th>Price</th>
+							   <th>Status</th>
+							     <th>Date</th>
+								   <th>Action</th>
+							  
+							</tr>
+						  </thead>
+						  <tbody>
+						  
+						  
+							<?php 
+				
+						$query_res= mysqli_query($db,"select * from users_orders where u_id='".$_SESSION['user_id']."'");
+												if(!mysqli_num_rows($query_res) > 0 )
+														{
+															echo '<td colspan="6"><center>You have No orders Placed yet. </center></td>';
+														}
+													else
+														{			      
+										  
+										  while($row=mysqli_fetch_array($query_res))
+										  {
+						
+							?>
+												<tr>	
+														 <td data-column="Item"> <?php echo $row['title']; ?></td>
+														  <td data-column="Quantity"> <?php echo $row['quantity']; ?></td>
+														  <td data-column="price">💰<?php echo $row['price']; ?></td>
+														   <td data-column="status"> 
+														   <?php 
+																			$status=$row['status'];
+																			if($status=="" or $status=="NULL")
+																			{
+																			?>
+																			<button type="button" class="btn btn-info"><span class=""  aria-hidden="true" >⏰</span> Order placed </button>
+																		   <?php 
+																			  }
+																			   if($status=="in process")
+																			 { ?>
+																				<button type="button" class="btn btn-warning"><span class=""  aria-hidden="true" >🚚</span> Delivered !</button>
+																			<?php
+																				}
+																			if($status=="closed")
+																				{
+																			?>
+																			 <button type="button" class="btn btn-success" ><span  class="" aria-hidden="true">✅</span> Paid</button> 
+																			<?php 
+																			} 
+																			?>
+																			<?php
+																			if($status=="rejected")
+																				{
+																			?>
+																			 <button type="button" class="btn btn-danger"> <i class="fa fa-close"></i> Cancelled</button>
+																			<?php 
+																			} 
+																			?>
+														   
+														   
+														   
+														   
+														   
+														   
+														   </td>
+														  <td data-column="Date"> <?php echo $row['date']; ?></td>
+														   <td data-column="Action"> <a href="delete_orders.php?order_del=<?php echo $row['o_id'];?>" onclick="return confirm('Are you sure you want to cancel your order?');" class="btn btn-danger btn-flat btn-addon btn-xs m-b-10"><i class="" style="font-size:16px"></i>🗑️</a> 
+															</td>
+														 
+												</tr>
+												
+											
+														<?php }} ?>					
+							
+							
+										
+						
+						  </tbody>
+					</table>
+						
+					
+                                    
+                                </div>
+                           
+                            </div>
+                         
+                            
+                                
+                            </div>
+                          
+                          
+                           
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+
+			<footer class="footer">
+            <div class="container">
+                
+          
+                <div class="bottom-footer">
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-3 payment-options color-gray">
+                            <h5 style="font-family:myFirstFont">ជម្រើសបង់ប្រាក់</h5>
+                            <ul>
+                                <li>
+                                    <a href="#"> <img src="images/paypal.png" alt="Paypal"> </a>
+                                </li>
+                                <li>
+                                    <a href="#"> <img src="images/mastercard.png" alt="Mastercard"> </a>
+                                </li>
+                                <li>
+                                    <a href="#"> <img src="images/khqr.png" alt="KH QR" width="45" height="25"> </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-xs-12 col-sm-4 address color-gray">
+                                    <h5 style="font-family:myFirstFont">អាស័យដ្ឋាន</h5>
+                                    <p style="font-family:myFirstFont">កម្ពុជា , ក្រុងប៉ោយប៉ែត</p>
+                                    <h5 style="font-family:myFirstFont">លេខទរសព្វ: <a style="font-family:myFirstFont"> +855 713323326</a></a></h5> </div>
+                                <div class="col-xs-12 col-sm-5 additional-info color-gray">
+                                    <h5 style="font-family:myFirstFont">ពត៌មានបន្ថែម</h5>
+                                   <p style="font-family:myFirstFont">អរគុណសម្រាប់ការគាំទ្រហាងយើងខ្ញុំ។</p>
+                                </div>
+                    </div>
+                </div>
+          
+            </div>
+        </footer>
+        
+        </div>
+  
+    
+    <script src="js/jquery.min.js"></script>
+    <script src="js/tether.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/animsition.min.js"></script>
+    <script src="js/bootstrap-slider.min.js"></script>
+    <script src="js/jquery.isotope.min.js"></script>
+    <script src="js/headroom.js"></script>
+    <script src="js/foodpicky.min.js"></script>
+</body>
+
+</html>
+<?php
+}
+?>
